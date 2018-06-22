c9fc1583db0ed374669c85e129b7cf97819fffce
Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>
2018-06-22 20:10:56 -0300

    Fixed post crete error: added post_type selector.
    Co-authored-by: Andre Hayashi <andre.hayashi@usp.br>
    Co-authored-by: Vinicius Agostini <Vinicius.agostini19@gmail.com>
    Co-authored-by: Marcelo Trylesinski <marcelo.usp@outlook.com>

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :get_subjects, only: [:index, :show, :new, :edit]
+  before_action :get_subjects #, only: [:index, :show, :new, :edit]
 
   # GET /posts
   def index
@@ -30,6 +30,8 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to @post, notice: 'Post was successfully created.'
     else
+      logger.debug "Post error: #{ puts @post.errors }"
+      logger.debug "Post error: #{@post.attributes.inspect}"
       render :new
     end
   end
