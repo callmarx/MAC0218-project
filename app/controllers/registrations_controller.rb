f59c8caa231e959f137411b2e94f0fe036ebcfeb
Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>
2018-07-02 23:35:37 -0300

    more improves on profile view.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,11 @@
 class RegistrationsController < Devise::RegistrationsController
 
+  protected
+
+  def after_update_path_for(resource)
+     profile_path(resource)
+  end
+
   private
 
   def sign_up_params
